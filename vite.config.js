24cc25cf4016f24487f8c5662fe49ce3ec5d860c
Dirceusljr <dirceusljr@gmail.com>
2024-08-09 15:07:26 -0300

    feat: :sparkles: add: initial project

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,  // Permite que o Vite escute em todas as interfaces.
+    port: 3000,  // Define a porta que será exposta pelo Docker.
+    watch: {
+      usePolling: true,  // Necessário para ambientes Docker para garantir que as mudanças sejam capturadas.
+    }
+  }
+});
+
