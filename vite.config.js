5d42884c067ca7c2d51145da7ec29d90c4993ae1
Dirceusljr <dirceusljr@gmail.com>
2024-08-13 20:22:18 -0300

    feat: :sparkles: add: changing port

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,  // Permite que o Vite escute em todas as interfaces.
-    port: 3000,  // Define a porta que será exposta pelo Docker.
+    port: 5173,  // Define a porta que será exposta pelo Docker.
     watch: {
       usePolling: true,  // Necessário para ambientes Docker para garantir que as mudanças sejam capturadas.
     }
