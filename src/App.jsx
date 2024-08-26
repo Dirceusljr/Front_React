9db17e889c2097a4977c3984853fe716afbe77a0
Dirceusljr <dirceusljr@gmail.com>
2024-08-26 18:22:58 -0300

    feat: :sparkles: feat: Card List

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import { useEffect, useState } from "react"
-import Card from "./components/CardList/Card"
 import Carousel from "./components/Carousel"
 import Header from "./components/Header"
 import NavBar from "./components/Navbar"
-import { getProducts } from "./services/products"
+import CarList from "./components/CardList"
 
 function App() {
-
-  const [product, setProduct] = useState({})
-
-  const getAllProducts = async () => {
-    const response = await getProducts()
-    setProduct(response)
-  }
-
-  useEffect(()=> {
-    getAllProducts()
-  }, [])
 
   return (
     <div className="bg-lightGray h-screen ">
       <Header />
       <NavBar />
       <Carousel />
-      <Card {...product} />
+      <CarList />
     </div>
   )
 }
